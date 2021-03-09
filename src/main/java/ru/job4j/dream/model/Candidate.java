f9d21030d2a495b94215b403cf8f33dc89cd4396
Aleksey Krylov <elsite2015@yandex.ru>
2021-03-09 15:19:51 +0400

    1. PsqlStore. [#476964]

--- src/main/java/ru/job4j/dream/model/Candidate.java
+++ src/main/java/ru/job4j/dream/model/Candidate.java
@@ -44,4 +44,9 @@ public class Candidate {
     public int hashCode() {
         return Objects.hash(id, name);
     }
+
+    @Override
+    public String toString() {
+        return "Candidate{" + "id=" + id + ", name='" + name + '\'' + '}';
+    }
 }
---
+++ src/main/java/ru/job4j/dream/store/PsqlMain.java
@@ -0,0 +1,22 @@
+package ru.job4j.dream.store;
+
+import ru.job4j.dream.model.Candidate;
+import ru.job4j.dream.model.Post;
+
+public class PsqlMain {
+    public static void main(String[] args) {
+        Store store = PsqlStore.instOf();
+        store.save(new Post(0, "Java Job"));
+        store.save(new Post(1, "Java Job"));
+        for (Post post : store.findAllPosts()) {
+            System.out.println(post.getId() + " " + post.getName());
+        }
+        System.out.println(store.findById(1));
+        store.save(new Candidate(0, "Candidate for Java Job"));
+        store.save(new Candidate(1, "Candidate for Java Job2"));
+        for (Candidate can : store.findAllCandidates()) {
+            System.out.println(can.getId() + " " + can.getName());
+        }
+        System.out.println(store.findByIdCan(1));
+    }
+}
